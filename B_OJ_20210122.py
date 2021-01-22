d0380d638372c6963543de7a45ae23f07bb5030d
ongeee <onji6@naver.com>
2021-01-22 17:36:38 +0900

    20210122

---
+++ B_OJ_20210122.py
@@ -0,0 +1,36 @@
+# Baekjoon Online Judge
+
+# 20210122
+
+# 5-1 (10818)
+N = input()
+N = int(N)
+
+M = map(int, input().split())
+M = list(M)
+    
+print(min(M), end=' ')
+print(max(M))
+
+# 5-2 (2562)
+N = []
+for i in range(9):
+    
+    K = input()
+    K = int(K)
+    
+    N.append(K)
+    
+print(max(N))
+print(int(N.index(max(N))+1))
+    
+# 5-3 (2577)
+A = int(input())
+B = int(input())
+C = int(input())
+
+mul = list(str(A*B*C))
+
+for i in range(10):
+    
+    print(mul.count(str(i)))
