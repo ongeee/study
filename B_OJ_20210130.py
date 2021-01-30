e752a19489513d6a435e5d43e776ee443643b39f
ongeee <onji6@naver.com>
2021-01-30 23:54:48 +0900

    20210130

--- B_OJ_20210130.py
+++ B_OJ_20210130.py
@@ -15,7 +15,31 @@ if (B >= C):
 else: 
     print(int(A // (C-B))+1)
     
-# 8-2 (2292)
+# 8-2 (2292) [RE]
+N = input()
+
+N = int(N)
+        
+num = 1
+six = 6
+K = 1
+if (N==1):
+    print(num)
+
+else:
+    while True:
+        
+        K = K + six
+        
+        if (N<=K):
+            num = num + 1 
+            print(num)
+            
+            break
+            
+        else:
+            num = num + 1
+            six = six + 6        
 
 # 8-3 (1193)
 
@@ -69,75 +93,14 @@ for i in range(N):
 # 8-7 (2839)
 
 # 8-8 (10757)
-
-# 8-9 (1011) 
-    # 8-1 (1712) [RE]
-A, B, C = input().split()
+A, B = input().split()
 
 A = int(A)
 B = int(B)
-C = int(C)
 
-if (B >= C):
-    print('-1')
-    
-else: 
-    print(int(A // (C-B))+1)
-    
-# 8-2 (2292)
-
-# 8-3 (1193)
-
-# 8-4 (2869) [RE]
-A, B, V = input().split()
-
-A = int(A)
-B = int(B)
-V = int(V)
-
-if ((V-B) % (A-B) != 0):
-    print(((V-B) // (A-B))+1)
-    
-else:
-    print(((V-B) // (A-B)))
-
-# 8-5 (10250) [RE]
-N = input()
-
-N = int(N)
-
-for i in range(N):
-    
-    H, W, N = input().split()
-    
-    H = int(H)
-    W = int(W)
-    N = int(N)
-    
-    K = N % H
-    
-    if K ==0:
-        floor = H
-        num = (N // H)
-    
-    else:
-        floor = K
-        num = (N // H) + 1
-        
-    
-    if (num<10):
-        num = '0' + str(num)
-        
-    floor = str(floor)
-    num = str(num)
-    
-    print(floor + num)
-
-# 8-6 (2775)
-
-# 8-7 (2839)
-
-# 8-8 (10757)
+print(A+B)
 
 # 8-9 (1011) 
+     
+        
     
